3cd844f404075463b1664c54a592c04379bfa997
MD. yasin Arafat Mubin <mubinulislam14@gmail.com>
2025-11-21 22:19:36 +0600

    upate ui

--- src/Components/ProjectDetails.jsx
+++ src/Components/ProjectDetails.jsx
@@ -17,9 +17,9 @@ const ProjectDetails = () => {
       style={{ backgroundImage: `url(${full_bg})` }}
     >
       <div
-  className=" lg:px-20 md:px-16 px-1 mx-auto p-6 bg-transparent backdrop-blur-sm container pb-20 h-full w-full"
-  id="children"
->
+        className=" lg:px-20 md:px-16 px-1 mx-auto p-6 bg-transparent backdrop-blur-sm container pb-20 h-full w-full"
+        id="children"
+      >
         <h2 className="text-4xl pt-20 font-bold text-white">{project.title}</h2>
         <p className="text-white mt-4">{project.description}</p>
         <pre className=" bg-red-900 bg-opacity-30 text-white lg:p-4 p-1 rounded-lg mt-4 lg:text-sm md:text-[13px] text-[10px]">
@@ -38,24 +38,24 @@ const ProjectDetails = () => {
         </div> */}
         <Link
           to={project.live_link}
-          target="_blank" 
+          target="_blank"
           className="btn bg-red-600 text-white border-none m-3 hover:bg-sky-600 transition"
         >
           Visit Project
         </Link>
-        {project.github_link && <Link
-          to={project.github_link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="btn btn-outline  text-red-600 m-3 hover:bg-sky-600 transition"
-        >
-          Visit Github
-        </Link>}
-        
+        {project.github_link && (
+          <Link
+            to={project.github_link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-outline  text-red-600 m-3 hover:bg-sky-600 transition"
+          >
+            Visit Github
+          </Link>
+        )}
       </div>
     </div>
   );
 };
 
 export default ProjectDetails;
-
